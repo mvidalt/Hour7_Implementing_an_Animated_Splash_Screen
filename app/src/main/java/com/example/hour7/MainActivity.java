05ed3bf76b92aa2ffd9a1388f03bdfaaeae2ff1a
mvidalt <mvidalt@cifpfbmoll.eu>
2023-11-24 20:35:43 +0100

    Añadimos animaciones

--- app/src/main/java/com/example/hour7/MainActivity.java
+++ app/src/main/java/com/example/hour7/MainActivity.java
@@ -1,7 +1,15 @@
 package com.example.hour7;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.LayoutAnimationController;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,59 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash);
+
+        TextView textView = findViewById(R.id.TextViewTopTitle);
+
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.fade_in);
+
+        textView.startAnimation(animation);
+
+        Animation spinin = AnimationUtils.loadAnimation(this, R.anim.custom_anim);
+        LayoutAnimationController controller =
+                new LayoutAnimationController(spinin);
+        TableLayout table = findViewById(R.id.TableLayout01);
+        for (int i = 0; i < table.getChildCount(); i++) {
+            TableRow row = (TableRow) table.getChildAt(i);
+            row.setLayoutAnimation(controller);
+        }
+
+        Animation fade2 = AnimationUtils.loadAnimation(this, R.anim.fade_in2);
+        fade2.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                startActivity(new Intent(MainActivity.this, MainActivity.class));
+                MainActivity.this.finish();
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
+        TextView textViewBottom = findViewById(R.id.TextViewBottomTitle);
+        textViewBottom.startAnimation(fade2);
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        // Stop the animation
+        TextView logo1 = findViewById(R.id.TextViewTopTitle);
+        logo1.clearAnimation();
+        TextView logo2 = findViewById(R.id.TextViewBottomTitle);
+        logo2.clearAnimation();
+
+        TableLayout table = findViewById(R.id.TableLayout01);
+        for (int i = 0; i < table.getChildCount(); i++) {
+            TableRow row = (TableRow) table.getChildAt(i);
+            row.clearAnimation();
+        }
     }
 }
